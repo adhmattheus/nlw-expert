52c104214b490d30ace00d97c2d510833ed16772
adhmattheus <mattheusadhonnaysf@hotmail.com>
2024-03-08 13:02:24 -0300

    aplicando resposividade e funcão de criar nota por audio e deletar nota

--- react/nlw_experts_notes/src/app.tsx
+++ react/nlw_experts_notes/src/app.tsx
@@ -1,10 +1,11 @@
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 import logo from './assets/logo-nlw-expert.svg'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { Note } from './types/Note'
 
 export function App() {
+  const [search, setSearch] = useState("");
   const [notes, setNotes] = useState<Note[]>(() => {
     const notesOnStorage = localStorage.getItem('notes')
 
@@ -28,8 +29,29 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter((note) => {
+      return note.id !== id;
+    });
+
+    setNotes(notesArray);
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value;
+    setSearch(query);
+  }
+
+  const filteredNotes = search !== ""
+    ? notes.filter((note) =>
+      note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+    )
+    : notes;
+
+
   return (
-    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+    <div className='mx-auto max-w-6xl my-12 space-y-6 px-5'>
       <img src={logo} alt='NLW Expert' />
 
       <form className='w-full'>
@@ -37,16 +59,17 @@ export function App() {
           type="text"
           placeholder='Busque suas notas...'
           className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+          onChange={handleSearch}
         />
       </form>
 
       <div className='h-px bg-slate-700' />
 
-      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard onNoteCreated={onNoteCreated} />
 
-        {notes.map(notes => {
-          return <NoteCard key={notes.id} note={notes} />
+        {filteredNotes.map(notes => {
+          return <NoteCard key={notes.id} note={notes} onNoteDeleted={onNoteDeleted} />
         })}
 
       </div>
--- react/nlw_experts_notes/src/types/noteCardProps.ts
+++ react/nlw_experts_notes/src/types/noteCardProps.ts
@@ -1,6 +1,8 @@
 export interface NoteCardProps {
   note: {
+    id: string,
     date: Date
     content: string
   }
+  onNoteDeleted: (id: string) => void
 }
